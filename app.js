be7600276f80d3916522c73274d5fcbffe11f113
scottlexium <scottlexium@gmail.com>
2022-03-31 21:29:19 +0100

    dotend

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const fs = require('fs');
+const dotenv = require('dotenv');
+dotenv.config();
 // create our express app
 const app = express()
 // middleware
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 // route
-const routes = require('./routes/Routes')
+const routes = require('./routes/Routes');
+const { defaultConfiguration } = require("express/lib/application");
 app.use('/', routes)
 //start server
-app.listen(3030, ()=>{
+const port = process.env.PORT||3030
+app.listen(port, ()=>{
     console.log("listeniing at port:3030")
 }) 
