93ccad6a5f9dec0e0f406bfea3d13080dbe53e3b
scottlexium <scottlexium@gmail.com>
2022-03-31 21:20:40 +0100

    second commit

--- routes/userController.js
+++ routes/userController.js
@@ -15,6 +15,20 @@ const getUserData = () => {
 }
 
 // get all 
+const routesHelp = ({
+    note: 'The following should guide you in testing',
+    get: '/user/list',
+    put: '/user/:id',
+    delete: '/user/delete/:id',
+    post:'/user/addUser',
+});
+const routesParameters = ({
+    postBody: 'username, password',
+    putBody: 'username, password',
+})
+controller.get('/', (req, res) => {
+    res.status(200).json({routesHelp, routesParameters})
+})
 controller.get('/user/list', (req, res) => {
     const accounts = getUserData()
     res.send(accounts)
@@ -37,11 +51,23 @@ controller.put('/user/:id', (req, res) => {
 controller.delete('/user/delete/:id', (req, res) => {
     console.log(req.params.id);
     fs.readFile(userDb, 'utf8', (err, data) => {
-      var existingUser = getUserData()
-      const userId = req.params['id'];
-      delete existingUser[userId]; 
-      saveUserData(existingUser);
-      res.send(`user with id ${userId} has been deleted`);
+        var existingUser = getUserData()
+        const userId = req.params['id'];
+        delete existingUser[userId];
+        saveUserData(existingUser);
+        res.send(`user with id ${userId} has been deleted`);
     }, true);
-  })
+});
+
+
+// using post method to add a user
+controller.post('/user/addUser', (req, res) => {
+
+    var existingUser = getUserData()
+    const newUserId = Math.floor(100000 + Math.random() * 900000)
+    existingUser[newUserId] = req.body
+    console.log(existingUser);
+    saveUserData(existingUser);
+    res.send({ success: true, msg: 'account added successfully' })
+})
 module.exports = controller
